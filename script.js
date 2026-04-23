9b600019a1ab6cd711b49da3519133e90720a7e1
Marcelo <264510676+MarceloSalas-boy@users.noreply.github.com>
2026-04-23 14:57:36 +0200

    Add getComputerChoice function

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-
-
-
-function getComputerChoice (x) {
-    return Rock Paper Scissors
+function getComputerChoice () {
+    let random = Math.floor(Math.random() * 3)
+    if (random === 0){
+        return "Rock"
+    } else if (random === 1) {
+        return "Paper"
+    } else {
+        return "Scissors"
+    }
 }
 
-console.log(getComputerChoice(3)) ;
-
-console.log(getComputerChoice(2)) ;
-
+console.log(getComputerChoice())
